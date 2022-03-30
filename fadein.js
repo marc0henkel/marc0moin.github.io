8df179a3a9b5ee84aa5a169094ec4e81ff8fa77c
marc0moin <marc0h@protonmail.com>
2022-03-30 17:33:40 +0200

    finish

---
+++ fadein.js
@@ -0,0 +1,7 @@
+var splash = document.querySelector('.splash');
+
+document.addEventListener('DOMContentLoaded', (e)=>{
+    setTimeout(()=>{
+        splash.classList.add('display-none')
+    }, 2000)
+})
